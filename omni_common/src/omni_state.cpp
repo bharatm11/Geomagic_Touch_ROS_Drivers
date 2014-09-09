d9ef60fec776d3b75dc38eb6bc0ac0ad8311f794
fsuarez6 <fsuarez6@gmail.com>
2014-09-09 10:23:08 -0400

    Added JointState publisher

--- omni_common/src/omni_state.cpp
+++ omni_common/src/omni_state.cpp
@@ -57,7 +57,8 @@ public:
   ros::NodeHandle n;
   ros::Publisher state_publisher;
   ros::Publisher pose_publisher;
-  ros::Publisher button_publisher;  
+  ros::Publisher button_publisher;
+  ros::Publisher joint_publisher;
   ros::Subscriber haptic_sub;
   std::string omni_name, ref_frame, units;
 
@@ -91,6 +92,12 @@ public:
     stream4 << omni_name << "/pose";
     std::string pose_topic_name = std::string(stream4.str());
     pose_publisher = n.advertise<geometry_msgs::PoseStamped>(pose_topic_name.c_str(), 1);
+    
+    //Publish on NAME/joint_states
+    std::ostringstream stream5;
+    stream5 << omni_name << "/joint_states";
+    std::string joint_topic_name = std::string(stream5.str());
+    joint_publisher = n.advertise<sensor_msgs::JointState>(joint_topic_name.c_str(), 1);
 
     state = s;
     state->buttons[0] = 0;
@@ -144,7 +151,7 @@ public:
     state->lock_pos[2] = omnifeed->position.z;
   }
 
-  void publish_omni_state() {   
+  void publish_omni_state() {
     // Build the state msg
     omni_msgs::OmniState state_msg;
     // Locked
@@ -166,6 +173,25 @@ public:
     // TODO: Append Current to the state msg
     state_msg.header.stamp = ros::Time::now();
     state_publisher.publish(state_msg);
+    
+    // Publish the JointState msg
+    sensor_msgs::JointState joint_state;
+    joint_state.header.stamp = ros::Time::now();
+    joint_state.name.resize(6);
+    joint_state.position.resize(6);
+    joint_state.name[0] = "waist";
+    joint_state.position[0] = -state->thetas[1];
+    joint_state.name[1] = "shoulder";
+    joint_state.position[1] = state->thetas[2];
+    joint_state.name[2] = "elbow";
+    joint_state.position[2] = state->thetas[3];
+    joint_state.name[3] = "yaw";
+    joint_state.position[3] = -state->thetas[4] + M_PI;
+    joint_state.name[4] = "pitch";
+    joint_state.position[4] = -state->thetas[5] - 3*M_PI/4;
+    joint_state.name[5] = "roll";
+    joint_state.position[5] = -state->thetas[6] - M_PI;
+    joint_publisher.publish(joint_state);
     
     // Build the pose msg
     geometry_msgs::PoseStamped pose_msg;
@@ -208,6 +234,8 @@ HDCallbackCode HDCALLBACK omni_state_callback(void *pUserData)
   hduMatrix transform;
   hdGetDoublev(HD_CURRENT_TRANSFORM, transform);
   hdGetDoublev(HD_CURRENT_JOINT_ANGLES, omni_state->joints);
+  hduVector3Dd gimbal_angles;
+  hdGetDoublev(HD_CURRENT_GIMBAL_ANGLES, gimbal_angles);
   // Notice that we are inverting the Z-position value and changing Y <---> Z
   // Position
   omni_state->position = hduVector3Dd(transform[3][0], -transform[3][2], transform[3][1]);
@@ -220,7 +248,7 @@ HDCallbackCode HDCALLBACK omni_state_callback(void *pUserData)
                              0.0,  0.0, 1.0, 0.0,
                              0.0,  0.0, 0.0, 1.0);
   rotation_offset.getRotationMatrix(rotation_offset);
-  omni_state->rot = hduQuaternion(rotation_offset * rotation);  
+  omni_state->rot = hduQuaternion(rotation_offset * rotation);
   // Velocity estimation
   hduVector3Dd vel_buff(0, 0, 0);
   vel_buff = (omni_state->position * 3 - 4 * omni_state->pos_hist1
@@ -266,8 +294,8 @@ HDCallbackCode HDCALLBACK omni_state_callback(void *pUserData)
   }
 
   float t[7] = { 0., omni_state->joints[0], omni_state->joints[1],
-      omni_state->joints[2] - omni_state->joints[1], omni_state->rot[0],
-      omni_state->rot[1], omni_state->rot[2] };
+      omni_state->joints[2] - omni_state->joints[1], gimbal_angles[0],
+      gimbal_angles[1], gimbal_angles[2] };
   for (int i = 0; i < 7; i++)
     omni_state->thetas[i] = t[i];
   return HD_CALLBACK_CONTINUE;
