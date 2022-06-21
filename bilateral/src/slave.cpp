b93c89eb496f2e7815515b98baa48be95dbebb1d
Daiki Kusuyama (X1Y) <daiki.milky.kinako@gmail.com>
2022-06-21 17:48:03 +0900

    master/pos.xでslave/force.xが動くように

--- bilateral/src/slave.cpp
+++ bilateral/src/slave.cpp
@@ -27,24 +27,29 @@ private:
     ros::Subscriber m_sub;
 
 public:
-    std::string omni_name;
+    std::string omni_name_master;
+    std::string omni_name_slave;
     BilateralController()
     {
-        ros::param::param(std::string("~omni_name"), omni_name, std::string("phantom"));
+        ros::param::param(std::string("/omni_name_master"), omni_name_master, std::string("/phantom_master/phantom"));
+        ros::param::param(std::string("/omni_name_slave"), omni_name_slave, std::string("/phantom_slave/phantom"));
         std::ostringstream stream1;
-        stream1 << omni_name << "/force_feedback";
+        stream1 << omni_name_slave << "/force_feedback";
         std::string state_topic_name = std::string(stream1.str());
         m_pub = m_nh.advertise<omni_msgs::OmniFeedback>(state_topic_name.c_str(), 1);
 
         //Subscribe to NAME/force_feedback
         std::ostringstream stream2;
-        stream2 << omni_name << "/pose";
+        stream2 << omni_name_master << "/pose";
         std::string force_feedback_topic = std::string(stream2.str());
+
+        ROS_INFO("forceeeeeeeee: %s", force_feedback_topic.c_str());
         m_sub = m_nh.subscribe(force_feedback_topic.c_str(), 1, &BilateralController::force_callback, this);
     }
 
     void force_callback(const geometry_msgs::PoseStamped::ConstPtr& pose)
     {
+        ROS_INFO("hoge");
         ////////////////////Some people might not like this extra damping, but it
         ////////////////////helps to stabilize the overall force feedback. It isn't
         ////////////////////like we are getting direct impedance matching from the
@@ -55,12 +60,13 @@ public:
         double master_z = pose->pose.position.z;
 
         omni_msgs::OmniFeedback force_msg;
-        force_msg.force.x = master_x;
+        force_msg.force.x = master_x * (-5.);
         force_msg.force.y = 0.0;
         force_msg.force.z = 0.0;
         force_msg.position.x = 0.0;
         force_msg.position.y = 0.0;
         force_msg.position.z = 0.0;
+        m_pub.publish(force_msg);
     }
 };
 
