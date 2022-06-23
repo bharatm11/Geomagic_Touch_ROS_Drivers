11c5b2ada261c03b0a1d1a975969b5555bf7de44
poyotamu000 <miki@jsk.imi.i.u-tokyo.ac.jp>
2022-06-24 02:07:02 +0900

    キレそう

--- bilateral/src/slave.cpp
+++ bilateral/src/slave.cpp
@@ -1,58 +1,55 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/Wrench.h>
-#include <geometry_msgs/WrenchStamped.h>
-#include <urdf/model.h>
+// #include <geometry_msgs/Wrench.h>
+// #include <geometry_msgs/WrenchStamped.h>
+// #include <urdf/model.h>
 
 #include <string.h>
-#include <stdio.h>
-#include <math.h>
-#include <assert.h>
-#include <sstream>
+// #include <stdio.h>
+// #include <math.h>
+// #include <assert.h>
+// #include <sstream>
 
 //#include "omni_msgs/OmniButtonEvent.h"
 #include "omni_msgs/OmniFeedback.h"
 //#include "omni_msgs/OmniState.h"
-#include "geometry_msgs/PoseStamped.h"
 
-float prev_time;
-int calibrationStyle;
+// float prev_time;
+// int calibrationStyle;
 
 class BilateralController
 {
 private:
+    std::string omni_name_master;
+    std::string omni_name_slave;
+
     ros::NodeHandle m_nh;
+    ros::NodeHandle m_pnh;
     ros::Publisher m_pub;
     ros::Subscriber m_sub;
 
 public:
-    std::string omni_name_master;
-    std::string omni_name_slave;
-    BilateralController()
-    {
-        ros::param::param(std::string("/omni_name_master"), omni_name_master, std::string("/phantom_master/phantom"));
-        ros::param::param(std::string("/omni_name_slave"), omni_name_slave, std::string("/phantom_slave/phantom"));
-        std::ostringstream stream1;
-        stream1 << omni_name_slave << "/force_feedback";
-        std::string state_topic_name = std::string(stream1.str());
-        m_pub = m_nh.advertise<omni_msgs::OmniFeedback>(state_topic_name.c_str(), 1);
-
-        //Subscribe to NAME/force_feedback
-        std::ostringstream stream2;
-        stream2 << omni_name_master << "/pose";
-        std::string force_feedback_topic = std::string(stream2.str());
-
-        ROS_INFO("forceeeeeeeee: %s", force_feedback_topic.c_str());
-        m_sub = m_nh.subscribe(force_feedback_topic.c_str(), 1, &BilateralController::force_callback, this);
+    BilateralController() : m_pnh("~") {
+        if (!m_pnh.getParam("/omni_name_master", omni_name_master)) {
+          ROS_FATAL("'omni_name_master' is not set");
+        }
+        if (!m_pnh.getParam("omni_name_slave", omni_name_slave)) {
+          ROS_FATAL("'omni_name_slave' is not set");
+        }
+        m_pub = m_nh.advertise<omni_msgs::OmniFeedback>(omni_name_slave + "/force_feedback", 1);
+        m_sub = m_nh.subscribe(omni_name_master + "/pose", 1, &BilateralController::force_callback, this);
     }
 
     void force_callback(const geometry_msgs::PoseStamped::ConstPtr& pose)
     {
         ROS_INFO("hoge");
-        ////////////////////Some people might not like this extra damping, but it
-        ////////////////////helps to stabilize the overall force feedback. It isn't
-        ////////////////////like we are getting direct impedance matching from the
-        ////////////////////omni anyway
+
+        ///////////////////////////////////////////////////////////
+        // Some people might not like this extra damping, but it
+        // helps to stabilize the overall force feedback. It isn't
+        // like we are getting direct impedance matching from the
+        // omni anyway
+        ///////////////////////////////////////////////////////////
 
         double master_x = pose->pose.position.x;
         // double master_y = pose->pose.position.y;
@@ -71,15 +68,10 @@ public:
 
 int main(int argc, char** argv)
 {
-    ////////////////////////////////////////////////////////////////
-    // Init ROS
-    ////////////////////////////////////////////////////////////////
     ros::init(argc, argv, "bilateral_slave");
-    BilateralController bi;
-
+    ROS_INFO("Start bilateral slave node ...");
+    BilateralController bilateral_controller;
     ros::spin();
-
-    ROS_INFO("Ending Session....");
-
-    return 0;
+    ROS_INFO("End bilateral slave node ...");
+    return EXIT_SUCCESS;
 }
