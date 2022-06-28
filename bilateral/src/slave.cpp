0b25f74ce1df4138e697fbd45342fdd3f4e7836c
Daiki Kusuyama (X1Y) <daiki.milky.kinako@gmail.com>
2022-06-28 20:18:24 +0900

    [dev]slaveのpositionを追従させるように

--- bilateral/src/slave.cpp
+++ bilateral/src/slave.cpp
@@ -1,20 +1,34 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <string.h>
+#include "omni_msgs/OmniFeedback.h"
 #include "slave.hpp"
 
-void BilateralController::force_callback(const geometry_msgs::PoseStamped::ConstPtr& pose)
-{
-    double master_x = pose->pose.position.x;
-    // double master_y = pose->pose.position.y;
-    // double master_z = pose->pose.position.z;
+double BilateralController::positionController(
+    double ref, double x, double k)
 
+{
+    return k * (ref - x);  // TODO: ディジタル制御器かく
+}
+
+
+void BilateralController::forceControl()
+{
+    geometry_msgs::Point master_pos = this->getMasterPose().position;
+    geometry_msgs::Point slave_pos = this->getSlavePose().position;
+    std::vector<double> params = this->getParams();
     omni_msgs::OmniFeedback force_msg;
-    force_msg.force.x = master_x * (-5.);
-    force_msg.force.y = 0.0;
-    force_msg.force.z = 0.0;
+    // phantomの場合、forceをかける方向はencの向きと逆
+    // A0Bにおいては各軸について符号あわせる
+    force_msg.force.x = this->positionController(master_pos.x, slave_pos.x, params.at(0));
+    force_msg.force.y = this->positionController(master_pos.y, slave_pos.y, params.at(1));
+    force_msg.force.z = this->positionController(master_pos.z, slave_pos.z, params.at(2));
     force_msg.position.x = 0.0;
     force_msg.position.y = 0.0;
     force_msg.position.z = 0.0;
     m_pub.publish(force_msg);
 }
+
 
 int main(int argc, char** argv)
 {
